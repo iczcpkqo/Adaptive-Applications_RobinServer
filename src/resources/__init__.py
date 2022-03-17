5e622e5c92bc60afa41939517cc81ac501d15088
PathakPratik <rajpratik10@gmail.com>
2022-03-17 19:40:43 +0000

    added mongo db

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource
 from src.response import Response
+from pymongo import MongoClient
 
 class HelloWorld(Resource):
 	def __init__(self, **kwargs):
 		self.db = kwargs['db']
+
+		# Connect to your MongoDB cluster
+		client = MongoClient(self.db._connection_string)
+		self.curr_db = client['sample_analytics']
 	
 	def get(self):
-		print(f"Hello World")
-		return Response.send_json_200("Hello World!")
+		cust = self.curr_db.customers.find_one({'email': "arroyocolton@gmail.com"})
+		return Response.send_json_200({"Customer": cust["name"]})
