1f82866b891919970959527a84094805c1f6a78f
PathakPratik <rajpratik10@gmail.com>
2022-04-27 23:53:57 +0100

    added update model api

--- src/controllers/UserModel.py
+++ src/controllers/UserModel.py
@@ -35,7 +35,48 @@ def UserModelBlueprint(database):
             database["userModel"].insert_one(result)
             return jsonify({"message": "Success"}), 200
         except Exception as e:
-            return jsonify({"main_message": "Something went wrong!!", "detail_message": str(e)}), 500
+            return jsonify({"main_message": "Something went wrong!!", "detail_message": str(e)}), 400
+
+    # Update Model Schema Payload
+    class UpdateModelSchema(Schema):
+        UserId = fields.Integer(required=True)
+        WorkingHours = fields.Nested(TimeRange(), required=False)
+        BreakTime = fields.Nested(TimeRange(), required=False)
+        TimePreference = fields.String(validate=validate.OneOf(['morning', 'afternoon', 'evening']), required=False)
+        WeatherPreference = fields.String(validate=validate.OneOf(['sunny', 'none']), required=False)
+
+    @UserModel.route("/update-model", methods=["POST"])
+    def UpdateModel():
+        # Unmarshal Payload
+        request_data = request.json
+
+        try:
+            result = UpdateModelSchema().load(request_data)
+            if "WorkingHours" in result: 
+                result["WorkingHours"]["StartTime"] = result["WorkingHours"]["StartTime"].isoformat()
+            if "WorkingHours" in result:
+                result["WorkingHours"]["EndTime"] = result["WorkingHours"]["EndTime"].isoformat()
+            if "BreakTime" in result:
+                result["BreakTime"]["StartTime"] = result["BreakTime"]["StartTime"].isoformat()
+            if "BreakTime" in result:
+                result["BreakTime"]["EndTime"] = result["BreakTime"]["EndTime"].isoformat()
+            UserId = result["UserId"]
+            del result["UserId"]
+        except ValidationError as err:
+            return jsonify(err.messages), 400
+
+        try:
+            resp = database["userModel"].update_one(
+                { 'UserId': UserId }, { '$set': result }
+            )
+            
+            if resp.matched_count > 0:
+                return jsonify({"message": "Success"}), 200
+            else:
+                return jsonify({"main_message": "Something went wrong!!", "detail_message": "The user does not exist"}), 400
+            
+        except Exception as e:
+            return jsonify({"main_message": "Something went wrong!!", "detail_message": str(e)}), 400
     
     return UserModel
 
